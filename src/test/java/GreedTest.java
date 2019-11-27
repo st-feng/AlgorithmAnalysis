d1cbcade301f0c84b6271f6fb0049373abe061e6
xcf0810 <xcf0810@126.com>
2019-11-27 19:39:22 +0800

    单源最短路径测试程序

---
+++ src/test/java/GreedTest.java
@@ -0,0 +1,10 @@
+import greed.MiniRounte;
+import org.junit.Test;
+
+public class GreedTest {
+    @Test
+    public void greedTest(){
+        MiniRounte miniRounte = new MiniRounte(1);
+        miniRounte.go();
+    }
+}
