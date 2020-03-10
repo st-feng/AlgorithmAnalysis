247ef25ab059b69db2910a5c37ca9fce5330988f
xcf0810 <xcf0810@126.com>
2020-03-10 19:15:48 +0800

    归档

--- src/main/java/trackback/MaxLoads.java
+++ src/main/java/trackback/MaxLoads.java
@@ -1,4 +1,19 @@
 package trackback;
 
 public class MaxLoads {
+//    传入轮船
+    int[] c;
+//    传入货物
+    int[] w;
+//    记录货物数量
+    int n;
+    MaxLoads(int[] c,int[] w){
+        this.n = w.length;
+        this.c = c;
+        this.w = w;
+    }
+
+    public void maxLoad(){
+
+    }
 }
